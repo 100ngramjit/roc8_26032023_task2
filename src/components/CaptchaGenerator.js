ac7451a7a8730fece05dddd1547b250c7464e9db
100ngramjit <sangramjitdutta@gmail.com>
2023-03-26 19:04:12 +0530

    captcha generator function created

---
+++ src/components/CaptchaGenerator.js
@@ -0,0 +1,9 @@
+export function generateCaptcha() {
+  const chars =
+    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let result = "";
+  for (let i = 0; i < 6; i++) {
+    result += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return result;
+}
